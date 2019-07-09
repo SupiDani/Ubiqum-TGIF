4b1f1b53f3d8864a8c06dd77591a52b2dac2649c
SupiDani26 <daniel@siempre-palante.eu>
2019-07-09 17:56:17 +0200

    data uploaded /  styling missing / data filtering

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ let allStatesfiltered;
 let dropdown = document.getElementById("dropdown");
 
 createTable("congress-data", data);
-document.write(allStates);
+//document.write(allStates);
 console.log(allStates)
 createDropdown()
 console.log(allStates)
 console.log(allStatesfiltered)
+
+
+// create table for members of
 
 function createTable(x ,data) {
     let members = data.results[0].members;
@@ -16,12 +19,16 @@ function createTable(x ,data) {
     
     for (let i = 0; i < members.length; i++) {
                   
+        let a = document.createElement("a");
         let tr = document.createElement("tr");
+
         let count = document.createElement("td");
         count.textContent = i+1;
         let name = document.createElement("td");
         name.textContent = members[i].last_name + ", " + members[i].first_name + " ";
         name.append(members[i].middle_name||"");
+      
+        
         let party = document.createElement("td");
         party.textContent = members[i].party;
         let state = document.createElement("td");
@@ -31,11 +38,17 @@ function createTable(x ,data) {
         let voteswithparty = document.createElement("td");
         voteswithparty.textContent = members[i].votes_with_party_pct;
         tr.append(count, name, party, state, seniority, voteswithparty);
+        //a.append(count, name, party, state, seniority, voteswithparty);
+        //tr.appendChild(a);
         tbody.appendChild(tr);
-        allStates.push(members[i].state);   
+        allStates.push(members[i].state);
+        //a.href = "#";
+        //a.className = "tr";   
     } 
     
 }
+
+// create State dropdown menu
 
 function createDropdown(x ,data) {
     let dropdown = document.getElementById("dropdown");
@@ -46,48 +59,40 @@ function createDropdown(x ,data) {
         if (v > w) {return 1;}
         return 0;
     });
-    for (let i=0; i < allStates.length; i++) {
-        var allStatesfiltered = allStates.filter(function(i){
 
-            return allStates[i] == allStates[i+1];
-    
-        });
+    // create list with states
+    var allStatesfiltered = []; 
+    for (let i=0; i < allStates.length; i++) {
         
-        if(allStates[i] == allStates[i+1]){
-            delete allStates[i];
+
+        if(allStates[i] != allStates[i+1]){
+            allStatesfiltered.push(allStates[i]);
         }
     }
-    /*var allStatesfiltered = allStates.filter(function(i){
+   
 
-        return allStates[i] == allStates[i+1];
-
-    });  */
-    document.write(allStates);
-    document.write(allStatesfiltered);
-
+    // create dorpdown list
 
     
-    /*for (let i = 0; i < members.length; i++) {
+    for (let i = 0; i < allStatesfiltered.length; i++) {
                   
-        let tr = document.createElement("tr");
-        let count = document.createElement("td");
-        count.textContent = i+1;
-        let name = document.createElement("td");
-        name.textContent = members[i].last_name + ", " + members[i].first_name + " ";
-        name.append(members[i].middle_name||"");
-        let party = document.createElement("td");
-        party.textContent = members[i].party;
-        let state = document.createElement("td");
-        state.textContent = members[i].state;
-        let seniority = document.createElement("td");
-        seniority.textContent = members[i].seniority;
-        let voteswithparty = document.createElement("td");
-        voteswithparty.textContent = members[i].votes_with_party_pct;
-        tr.append(count, name, party, state, seniority, voteswithparty);
-        tbody.appendChild(tr);
-        allStates.push(members[i].state);   
+        //let li = document.createElement("li");
+        //li.textContent = allStatesfiltered[i];
+        //let a = document.createElement("a");
+        //let linkText = allStatesfiltered[i];
+        //a.appendChild(linkText);
+        //a.href = "#";
+        //a.class="dropdown-item";
+        let a = document.createElement("a");
+        let linkText = document.createTextNode(allStatesfiltered[i]);
+        a.appendChild(linkText);
+        a.href = "#";
+        a.className = "dropdown-item";
+        dropdown.append(a);
+        //dropdown.append(li);
+        
     } 
-    */
+    
 }
 
 
